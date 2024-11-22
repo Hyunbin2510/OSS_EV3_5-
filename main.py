56bcc065976afb8c8a39c1a0ad1d891d26daf612
Hyunbin Kim <khb2439@gmail.com>
2024-11-22 14:25:17 +0900

    그랩 모터 구현

--- main.py
+++ main.py
@@ -18,4 +18,42 @@ ev3 = EV3Brick()
 
 
 # Write your program here.
-ev3.speaker.beep()
+'''
+모터 정의
+'''
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.D)
+grab_arm = Motor(Port.B)
+shooting_arm = Motor(Port.C)
+
+wheel_diameter = 5.6
+axle_track = 115
+robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
+
+def motion1():
+    '''
+    팔을 최대한 위로 옮기기
+    '''
+    grab_arm.reset_angle(0)
+    grab_arm.run_until_stalled(-200,duty_limit=30)
+    grab_high_angle = grab_arm.angle()
+
+
+def motion2():
+    '''
+    중간높이 올리기 (벽에 걸리지 않게)
+    '''
+    motion3()
+    grab_arm.reset_angle(0)
+    grab_arm.run_angle(100,-45)
+def motion3():
+    '''
+    공을 잡기위해 팔을 최대한 아래로 옮기기
+    '''
+    grab_arm.run_until_stalled(200,duty_limit=30)
+    grab_low_angle = grab_arm.angle()
+
+motion1()
+motion2()
+# robot.straight(100)
+
